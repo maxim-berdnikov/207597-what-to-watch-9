490ed975cf5da1cebc05e7955a07b4e974289a7d
Maxim Berdnikov <m.radik25@gmail.com>
2022-03-10 22:10:22 +0300

    3.1 Добавил роуты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,26 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import Main from '../main/main';
+import SignIn from '../sign-in/sign-in';
+import MyList from '../my-list/my-list';
+import Film from '../film/film';
 
 type AppProps = { title: string; genre: string; year: number };
 
 function App({ title, genre, year }: AppProps): JSX.Element {
-  return <Main title={title} genre={genre} year={year} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main title={title} genre={genre} year={year} />}
+        />
+        <Route path={AppRoute.SignIn} element={<SignIn />} />
+        <Route path={AppRoute.MyList} element={<MyList />} />
+        <Route path={AppRoute.Film} element={<Film />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
