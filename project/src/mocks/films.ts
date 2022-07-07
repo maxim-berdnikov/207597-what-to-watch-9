b64ea6f33ccc2480022930b04c229892b3f3438b
Maxim Berdnikov <m.radik25@gmail.com>
2022-07-07 23:03:26 +0300

    add films

---
+++ project/src/mocks/films.ts
@@ -0,0 +1,106 @@
+export const FILFM = [
+  {
+    id: 1,
+    title: 'The Grand Budapest Hotel ',
+    genre: 'Drama',
+    year: 2014,
+    rating: 'Very good',
+    score: 8.9,
+    votes: 240,
+    desc: `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). 
+        Zero, a junior lobby boy, becomes Gustave's friend and protege.\n\nGustave prides himself on providing first-className service to the hotel's 
+        guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave 
+        finds himself the recipient of a priceless painting and the chief suspect in her murder.`,
+    director: 'Wes Anderson',
+    actors: 'Bill Murray, Edward Norton, Jude Law, Willem Dafoe and other',
+    img: '',
+    video:
+      'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+  },
+  {
+    id: 2,
+    title: 'Авиатор',
+    genre: 'Драма',
+    year: 2004,
+    rating: 'Хорошо',
+    score: 7.6,
+    votes: 145,
+    desc: 'История миллиардера Говарда Хьюза, который был одержим небом. Леонардо ДиКаприо в байопике с пятью «Оскарами»',
+    director: 'Мартин Скорсезе',
+    actors: 'Леонардо ДиКаприо',
+    img: 'https://avatars.mds.yandex.net/get-kinopoisk-image/4303601/2b5dcd82-df11-405b-bc8e-d49015e7c918/600x900',
+    video:
+      'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+  },
+  {
+    id: 3,
+    title: 'Богемская рапсодия',
+    genre: 'Биография',
+    year: 2018,
+    rating: 'Очень хорошо',
+    score: 8.0,
+    votes: 432,
+    desc: 'История легендарной группы Queen и ее основателя. Сенсационный байопик, который получил четыре «Оскара»',
+    director: 'Брайан Сингер',
+    actors: 'Рами Малек, Люси Бойнтон, Гвилим Ли, Бен Харди',
+    img: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/a8132729-68c2-4f03-9795-0d886db8ae38/600x900',
+    video:
+      'https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4',
+  },
+  // {
+  //     id: ,
+  //     title: "",
+  //     genre: "",
+  //     year: ,
+  //     rating: "",
+  //     score: ,
+  //     votes: ,
+  //     desc: "",
+  //     director: "",
+  //     actors: "",img: "",
+  //     video: "https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4"
+
+  // },
+  // {
+  //     id: ,
+  //     title: "",
+  //     genre: "",
+  //     year: ,
+  //     rating: "",
+  //     score: ,
+  //     votes: ,
+  //     desc: "",
+  //     director: "",
+  //     actors: "",img: "",
+  //     video: "https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4"
+
+  // },
+  // {
+  //     id: ,
+  //     title: "",
+  //     genre: "",
+  //     year: ,
+  //     rating: "",
+  //     score: ,
+  //     votes: ,
+  //     desc: "",
+  //     director: "",
+  //     actors: "",img: "",
+  //     video: "https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4"
+
+  // },
+  // {
+  //     id: ,
+  //     title: "",
+  //     genre: "",
+  //     year: ,
+  //     rating: "",
+  //     score: ,
+  //     votes: ,
+  //     desc: "",
+  //     director: "",
+  //     actors: "",img: "",
+  //     video: "https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4"
+
+  // }
+];
